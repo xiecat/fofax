b2f0ad27ef174eb2144373e153be78084e6aa9f6
FanqXu <fanqxu@gmail.com>
2021-12-15 16:15:57 +0800

    update ascii logo

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"fmt"
+
 	"github.com/fatih/color"
 )
 
@@ -15,16 +16,13 @@ var (
 func banner() {
 	magenta := color.New(color.FgMagenta).PrintfFunc()
 	fmt.Println("")
-	magenta("      _____     _____       _____     _____      __  __   \n")
-	magenta(`    /\_____\   ) ___ (    /\_____\   /\___/\   /\  /\  /\ ` + "\n")
-	magenta(`   ( (  ___/  / /\_/\ \  ( (  ___/  / / _ \ \  \ \ \/ / / ` + "\n")
-	magenta(`    \ \ \_   / /_/ (_\ \  \ \ \_    \ \(_)/ /   \ \  / /  ` + "\n")
-	magenta(`    / / /_\  \ \ )_/ / /  / / /_\   / / _ \ \   / /  \ \  ` + "\n")
-	magenta(`   / /____/   \ \/_\/ /  / /____/  ( (_( )_) ) / / /\ \ \ ` + "\n")
-	magenta(`   \/_/        )_____(   \/_/       \/_/ \_\/  \/__\/__\/ ` + "\n")
+	magenta("    ____      ____          \n")
+	magenta(`   / __/___  / __/___ __  __` + "\n")
+	magenta(`  / /_/ __ \/ /_/ __ ` + "`/ |/_/" + "\n")
+	magenta(` / __/ /_/ / __/ /_/ />  <  ` + "\n")
+	magenta(`/_/  \____/_/  \__,_/_/|_|  ` + "\n")
 	fmt.Println("")
 	g := color.New(color.FgGreen).PrintfFunc()
 	g(fmt.Sprintf("                                   { %s  Author:xiecat }\n", FoFaXVersion))
 	fmt.Println("")
-
 }
